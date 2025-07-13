d65dcba9240c2e50ddce39432f43d831ae1dd43c
gfj2e <gfj2e@outlook.com>
2025-07-13 11:52:19 -0500

    Renamed file and Added funcs
    Added build_url and get_genre functions

--- steam_deals_scraper.py
+++ steam_deals_scraper.py
@@ -26,10 +26,32 @@ def setup_driver():
     driver = webdriver.Chrome(options=options)
     return driver
 
+# Developed by Garrett: Get the genre to search for by the user
+def get_genre():
+    print("Available Genres: ")
+    for genre in GENRE_TAGS.keys():
+        print(f"- {genre}")
+        
+    genre_opt = input("Choose your genre you want to search: ")
+    while genre_opt not in GENRE_TAGS.keys():
+        genre_opt = input("Please choose a valid genre: ")
+        
+    return genre_opt
+
+# Developed by Garrett: Returns url string of genre and next page
+def build_url(genre, page):
+    return f"https://store.steampowered.com/search/?specials=1&tags={genre}&page={page}"
+
+def search_deals(genre, driver):
+    deals = []
+    pass
+
 def main():
+    genre = get_genre()
+    url = build_url(genre, 3)
     driver = setup_driver()
-    driver.get("https://store.steampowered.com/search/?specials=1")
-    print(driver.title)
+    driver.get(url)
+    print(driver.current_url)
     time.sleep(3)
     driver.quit()
     print("Driver Closed.")
