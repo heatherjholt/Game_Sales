c11c0521e7425b1a6e0c0132fdeb1901af2b9136
gfj2e <gfj2e@outlook.com>
2025-07-13 09:43:26 -0500

    Driver Init
    Initialize the Chrome driver with the function setup_driver(). This will allow the script to perform automated tasks with Steam.

--- game_deal_scraper.py
+++ game_deal_scraper.py
@@ -1,3 +1,35 @@
-print("Hello World")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import bs4
+import time
 
-print("Boo")
+# Steam uses ids to represent their genres
+# Will be used to query for specific genres when scraping
+GENRE_TAGS = {
+    "action": 19,
+    "indie": 492,
+    "rpg": 122,
+    "Simulation": 599,
+    "Sports": 701,
+    "Early Access": 493,
+    "horror": 1667
+}
+
+STEAM_CSV_FILE = "steam.csv"
+RESULTS_PER_PAGE = 25
+
+# Developed by Garrett: Setup the driver for Chrome 
+# Run in headless mode
+def setup_driver():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+def main():
+    driver = setup_driver()
+    time.sleep(3)
+    driver.quit()
+    
+if __name__ == "__main__":
+    main()
