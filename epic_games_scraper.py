49bb862aff4f6420e075df88e08532eb01cda049
Gabby <gvadai91@icloud.com>
2025-07-15 09:41:45 -0500

    updating Epic games scraper

--- epic_games_scraper.py
+++ epic_games_scraper.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import time
 import csv
 import os
-import urllib.parse
+
 
 #Epic games genre tags
 GENRE_TAGS = {
@@ -21,3 +21,63 @@ GENRE_TAGS = {
     "puzzle": "Puzzle",
     "sports": "Sports"
 }
+
+# Epic CSV file for game deals
+EPIC_CSV_FILE = "epic.csv"
+
+# Setting up chrome driver 
+def setup_driver():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    print("Driver Initialized")
+    return driver
+
+def build_url(genre):
+    return f"https://store.epicgames.com/en-US/browse?sortBy=releaseDate&sortDir=DESC&tag={genre}&priceTier=tierDiscouted&category=Game&count=40&start=0"
+
+def search_deals(genre):
+    driver = setup_driver()
+    deals = []
+    
+    try:
+        
+        url = build_url(genre)
+        driver.get(url)
+        
+        wait = WebDriverWait(driver, 10)
+        
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.search_result_row")))
+        
+        time.sleep(1)
+        
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        rows = soup.select("a.search_result_row")
+        
+        if rows:
+            for row in rows:
+                title_element = row.select_one(".title")
+                discount_element = row.select_one(".discount_pct")
+                original_price_element = row.select_one(".discount_original_price")
+                discounted_price_element = row.select_one(".discount_final_price")
+                game_url = row.get("href", "").strip()
+
+                deal = {
+                    "title": title_element.text.strip() if title_element else "",
+                    "discount": discount_element.text.strip() if discount_element else "",
+                    "original price": original_price_element.text.strip() if original_price_element else "",
+                    "discount price": discounted_price_element.text.strip() if discounted_price_element else "",
+                    "url": game_url if game_url else ""
+                } 
+                deals.append(deal)
+            
+            print(f"Page {page}: Scrapped {len(rows)} deals.")
+        else:
+            print(f"No results on page {page}.")
+                
+    except NoSuchElementException:
+        driver.quit()
+    
+    driver.quit()
+    
+    return deals
