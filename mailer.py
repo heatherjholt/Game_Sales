a9cd744c554ef01775c091b49771c68026e10117
Gabby <gvadai91@icloud.com>
2025-07-21 11:34:19 -0500

    updating email
    updated email account for the project, using gmail with 2 factor auth and app password enabled

--- mailer.py
+++ mailer.py
@@ -4,10 +4,10 @@ from request_deals import top_deals
 
 
 port = 587
-smtp_server = "smtp.office365.com"
+smtp_server = "smtp.gmail.com"
 
-EMAIL_ADDRESS = "cheapsnake1@outlook.com"
-EMAIL_PASSWORD = "CoolKidz123"
+EMAIL_ADDRESS = "CheapSnake2025@gmail.com"
+EMAIL_PASSWORD = "nisd dmht nkqm rmuw"
 
 receiver_email = input("Enter Email: ")
 
@@ -19,10 +19,12 @@ def send_outlook_email(subject, body, to):
     msg.set_content(body)
 
     # Connect to Outlook SMTP server
-    with smtplib.SMTP('smtp.office365.com', 587) as smtp:
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
         smtp.starttls()  # Upgrade the connection to secure
+        smtp.ehlo()
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
-        print("Email sent via Outlook!") 
+        print("Email sent via Gmail!") 
 
 send_outlook_email("New Deals!", "testing", receiver_email)
